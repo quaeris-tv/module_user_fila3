6bb459df31b6bee95aed54a2071310788412a162
Malebestia <vair81@gmail.com>
2024-10-18 16:44:08 +0200

    refactor(Role.php): update Role model to use Spatie\Permission\Models\Role and add missing properties, methods and relationships

--- Models/Role.php
+++ Models/Role.php
@@ -1,35 +1,105 @@
 <?php
 
+/**
+ * ---.
+ */
+
 declare(strict_types=1);
 
 namespace Modules\User\Models;
 
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
+use Modules\Xot\Contracts\UserContract;
+use Modules\Xot\Datas\XotData;
+use Spatie\Permission\Models\Role as SpatieRole;
+
 /**
- * @property \Modules\Xot\Contracts\ProfileContract|null $creator
- * @property \Modules\Xot\Contracts\ProfileContract|null $updater
+ * Modules\User\Models\Role.
  *
- * @method static \Illuminate\Database\Eloquent\Builder|PermissionRole newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|PermissionRole newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|PermissionRole query()
+ * @property string                                                                    $uuid
+ * @property string|null                                                               $team_id
+ * @property string                                                                    $name
+ * @property string                                                                    $guard_name
+ * @property Carbon|null                                                               $created_at
+ * @property Carbon|null                                                               $updated_at
+ * @property Collection<int, \Modules\User\Models\Permission>                          $permissions
+ * @property int|null                                                                  $permissions_count
+ * @property Team|null                                                                 $team
+ * @property EloquentCollection<int, \Illuminate\Database\Eloquent\Model&UserContract> $users
+ * @property int|null                                                                  $users_count
  *
- * @property string                          $id
- * @property string|null                     $permission_id
- * @property string|null                     $role_id
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property string|null                     $updated_by
- * @property string|null                     $created_by
+ * @method static \Illuminate\Database\Eloquent\Builder|Role newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Role newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Role permission($permissions)
+ * @method static \Illuminate\Database\Eloquent\Builder|Role query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereGuardName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereTeamId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereUuid($value)
  *
- * @method static \Illuminate\Database\Eloquent\Builder|PermissionRole whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|PermissionRole whereCreatedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|PermissionRole whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|PermissionRole wherePermissionId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|PermissionRole whereRoleId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|PermissionRole whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|PermissionRole whereUpdatedBy($value)
+ * @property int $id
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereId($value)
+ *
+ * @property string|null $updated_by
+ * @property string|null $created_by
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereCreatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereUpdatedBy($value)
+ *
+ * @mixin Eloquent
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Role withoutPermission($permissions)
  *
  * @mixin \Eloquent
  */
-class PermissionRole extends BasePivot
+class Role extends SpatieRole
 {
+    use HasFactory;
+
+    // use HasUuids;
+
+    final public const ROLE_ADMINISTRATOR = 1;
+
+    final public const ROLE_OWNER = 2;
+
+    final public const ROLE_USER = 3;
+
+    /** @var string */
+    protected $connection = 'user';
+
+    // protected $fillable=['id','']
+
+    /**
+     * Get all of the teams the user belongs to.
+     */
+    public function team(): BelongsTo
+    {
+        $xotData = XotData::make();
+        /** @var class-string<Model> */
+        $teamClass = $xotData->getTeamClass();
+
+        return $this->belongsTo($teamClass);
+        /*
+        $pivotClass = $xot->getMembershipClass();
+        $pivot = app($pivotClass);
+        $pivotTable = $pivot->getTable();
+        $pivotDbName = $pivot->getConnection()->getDatabaseName();
+        $pivotTableFull = $pivotDbName.'.'.$pivotTable;
+
+        // $this->setConnection('mysql');
+        return $this->belongsToMany($xot->getTeamClass(), $pivotTableFull, null, 'team_id')
+            ->using($pivotClass)
+            ->withPivot('role')
+            ->withTimestamps()
+            ->as('membership');
+        */
+    }
 }
