50384e615a7773b8c0ceb33d5019c1c66b7a81b1
marco <marco.sottana@gmail.com>
2024-10-23 18:12:19 +0200

    🐛 (server.ts): change port variable case from lowercase port to uppercase PORT to improve semantics ✨ (server.ts): add support for process.env.PORT environment variable to be able to run app on a configurable port

---
+++ Resources/lang/it/fields.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'name' => 'Nome',
+    'email' => 'Email',
+    'created_at' => 'Creato il',
+    'updated_at' => 'Aggiornato il',
+    'role' => 'Ruolo',
+    'actions' => [
+        'attach_user' => 'Attacca utente',
+    ],
+];
