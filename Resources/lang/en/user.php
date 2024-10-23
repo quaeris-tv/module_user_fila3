b22ec788588739868d17934fe7a04e7567df8176
marco <marco.sottana@gmail.com>
2024-10-23 16:00:30 +0200

    .

---
+++ Resources/lang/en/user.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'actions' => [
+        'attach_user' => 'Attach User',
+        'associate_user' => 'Associate User',
+        'user_actions' => 'User Actions',
+        'view' => 'View',
+        'edit' => 'Edit',
+        'detach' => 'Detach',
+        'row_actions' => 'Actions',
+        'delete_selected' => 'Delete Selected',
+        'confirm_detach' => 'Are you sure you want to detach this user?',
+        'confirm_delete' => 'Are you sure you want to delete the selected users?',
+        'success_attached' => 'User successfully attached',
+        'success_detached' => 'User successfully detached',
+        'success_deleted' => 'Users successfully deleted',
+    ],
+    'fields' => [
+        'name' => 'Name',
+        'email' => 'Email',
+        'created_at' => 'Created At',
+        'updated_at' => 'Updated At',
+        'role' => 'Role',
+        'active' => 'Active',
+    ],
+    'filters' => [
+        'active_users' => 'Active Users',
+        'creation_date' => 'Creation Date',
+        'date_from' => 'From',
+        'date_to' => 'To',
+    ],
+    'messages' => [
+        'no_records' => 'No users found',
+        'loading' => 'Loading users...',
+        'search' => 'Search users...',
+    ],
+];
