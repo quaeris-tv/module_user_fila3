5985185d0092254e9d20f916443a4a7c334d8430
marco <marco.sottana@gmail.com>
2024-10-28 09:56:02 +0100

    .

---
+++ Resources/lang/en/notifications.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'password_changed_successfully' => [
+        'title' => 'Password changed successfully!',
+        'message' => 'Your password has been updated.',
+    ],
+];
