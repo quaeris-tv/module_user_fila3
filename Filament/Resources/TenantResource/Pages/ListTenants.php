316c5c7b78dfa1f10f7502183987e857ed2ffdfa
marco76tv <marco.sottana@gmail.com>
2024-10-29 11:52:52 +0100

    🔧 (UsersRelationManager.php): Remove unused code and methods to improve maintainability and readability 🔧 (ListTenants.php): Remove unused code and methods to improve maintainability and readability

--- Filament/Resources/TenantResource/Pages/ListTenants.php
+++ Filament/Resources/TenantResource/Pages/ListTenants.php
@@ -24,35 +24,8 @@ class ListTenants extends ListRecords
     public function getListTableColumns(): array
     {
         return [
-<<<<<<< HEAD
             TextColumn::make('id')->label(__('user::fields.id.label'))->searchable()->sortable(),
             TextColumn::make('name')->label(__('user::fields.name.label')),
-=======
-            TextColumn::make('id')->label(__('ID'))->searchable()->sortable(),
-            TextColumn::make('name')->label(__('Nome')),
-        ];
-    }
-
-    protected function getTableFilters(): array
-    {
-        return [
-            // Your filters here
-        ];
-    }
-
-    protected function getTableActions(): array
-    {
-        return [
-            TableActions\ViewAction::make(),
-            TableActions\EditAction::make(),
-        ];
-    }
-
-    protected function getTableBulkActions(): array
-    {
-        return [
-            TableActions\DeleteBulkAction::make(),
->>>>>>> eacae92a (fix(ListTenants): remove unused code and methods to improve maintainability and readability)
         ];
     }
 }
