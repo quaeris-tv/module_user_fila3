d4930ee0a361955f2cb7f434199697fba7fe2492
marco76tv <marco.sottana@gmail.com>
2024-10-29 09:39:04 +0100

    up

--- Models/Traits/HasTenants.php
+++ Models/Traits/HasTenants.php
@@ -18,10 +18,7 @@ use Modules\Xot\Datas\XotData;
  */
 trait HasTenants
 {
-    // public function teams(): BelongsToMany
-    // {
-    //    return $this->belongsToMany(Team::class);
-    // }
+
 
     public function canAccessTenant(Model $tenant): bool
     {
@@ -41,21 +38,12 @@ trait HasTenants
     public function tenants(): BelongsToMany
     {
         $xot = XotData::make();
-        $pivotClass = $xot->getTenantPivotClass();
-
-        $pivot = app($pivotClass);
-        $pivotTable = $pivot->getTable();
-        $pivotDbName = $pivot->getConnection()->getDatabaseName();
-        $pivotTableFull = $pivotDbName.'.'.$pivotTable;
-        $pivotFields = $pivot->getFillable();
         /** @var class-string<Model> */
         $tenant_class = $xot->getTenantClass();
 
         // $this->setConnection('mysql');
-        return $this->belongsToMany($tenant_class, $pivotTableFull, null, 'tenant_id')
-            ->using($pivotClass)
-            ->withPivot($pivotFields)
-            ->withTimestamps();
+        return $this->belongsToManyX($tenant_class, null, null, 'tenant_id')
+            ;
         // ->ddRawSql()
         // ->as('membership')
     }
