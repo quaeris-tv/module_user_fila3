85343fa3caa5c470fc0c15934ba3e6bddf70f2d2
marco76tv <marco.sottana@gmail.com>
2024-10-29 11:52:15 +0100

    ♻️ (PermissionRole.php): refactor casts method in PermissionRole model for better readability and maintainability ♻️ (Role.php): remove unused import PermissionRegistrar in Role model to clean up the code and improve code quality

--- Models/PermissionRole.php
+++ Models/PermissionRole.php
@@ -32,15 +32,15 @@ namespace Modules\User\Models;
  */
 class PermissionRole extends BasePivot
 {
-
     /** @return array<string, string> */
     public function casts(): array
-    {   
-        $parent=parent::casts();
-        $up=[
+    {
+        $parent = parent::casts();
+        $up = [
             'permission_id' => 'string',
             'role_id' => 'string',
         ];
-        return array_merge($parent,$up);
+
+        return array_merge($parent, $up);
     }
 }
