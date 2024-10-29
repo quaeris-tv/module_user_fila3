8a227ebbd4a3f7d2e3660b5cd9da3440e6b4f522
marco76tv <marco.sottana@gmail.com>
2024-10-29 13:39:54 +0100

    up

--- Filament/Resources/TenantResource/Pages/ListTenants.php
+++ Filament/Resources/TenantResource/Pages/ListTenants.php
@@ -21,305 +21,32 @@ use Modules\UI\Enums\TableLayoutEnum;
 use Modules\UI\Filament\Actions\Table\TableLayoutToggleTableAction;
 use Modules\User\Filament\Resources\TenantResource;
 use Modules\User\Models\User;
+use Modules\Xot\Filament\Traits\HasXotTable;
 
 class ListTenants extends ListRecords
 {
+    use HasXotTable;
     public TableLayoutEnum $layoutView = TableLayoutEnum::LIST;
 
     protected static string $resource = TenantResource::class;
 
-    protected function getTableHeaderActions(): array
-    {
-        return [
-            TableLayoutToggleTableAction::make(),
-        ];
-    }
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\CreateAction::make(),
-        ];
-    }
-
-    public function table(Table $table): Table
-    {
-        $tenantModel = static::getModel();
-        $tableName = app($tenantModel)->getTable();
-
-        // Check if the table exists
-        if (! $this->tableExists($tenantModel)) {
-            $this->notifyTableMissing($tableName);
-
-            return $this->configureEmptyTable($table);
-        }
-
-        return $this->configureTable($table);
-    }
-
-    protected function tableExists(string $model): bool
-    {
-        return app($model)->getConnection()->getSchemaBuilder()->hasTable(app($model)->getTable());
-    }
-
-    protected function notifyTableMissing(string $tableName): void
-    {
-        Notification::make()
-            ->title(__('Attenzione'))
-            ->body(__('La tabella :table non esiste', ['table' => $tableName]))
-            ->persistent()
-            ->warning()
-            ->send();
-    }
-
-    protected function configureEmptyTable(Table $table): Table
-    {
-        return $table
-            ->modifyQueryUsing(fn (Builder $query) => User::query()->where('id', null))
-            ->columns([
-                TextColumn::make('message')
-                    ->label(__('Messaggio'))
-                    ->default(__('La tabella tenants non è stata impostata. Per favore, configurala.'))
-                    ->html(), // Allows you to format the message if needed
-            ])
-            ->headerActions([]) // No actions
-            ->actions([]); // No footer actions
-    }
-
-    protected function configureTable(Table $table): Table
-    {
-        return $table
-            ->columns($this->layoutView->getTableColumns())
-            ->contentGrid($this->layoutView->getTableContentGrid())
-            ->headerActions($this->getTableHeaderActions())
-            ->filters($this->getTableFilters())
-            ->filtersLayout(FiltersLayout::AboveContent)
-            ->persistFiltersInSession()
-            ->actions($this->getTableActions())
-            ->bulkActions($this->getTableBulkActions())
-            ->actionsPosition(ActionsPosition::BeforeColumns)
-            ->defaultSort(column: 'created_at', direction: 'DESC');
-    }
-
-    public function getGridTableColumns(): array
-    {
-        return [
-            Stack::make($this->getListTableColumns()),
-        ];
-    }
 
     public function getListTableColumns(): array
     {
         return [
-            TextColumn::make('id')->label(__('ID'))->searchable()->sortable(),
-            TextColumn::make('name')->label(__('Nome')),
+            TextColumn::make('id')
+                ->label(__('ID'))->searchable()->sortable(),
+            TextColumn::make('name')
+                ->label(__('Nome')),
+            TextColumn::make('slug')
+                ->label(__('Slug'))
+                ->default(function($record){
+                    $record->generateSlug();
+                    $record->save();
+
+                }),
         ];
     }
 
-    protected function getTableFilters(): array
-    {
-        return [
-            // Your filters here
-        ];
-    }
 
-    protected function getTableActions(): array
-    {
-        return [
-            TableActions\ViewAction::make(),
-            TableActions\EditAction::make(),
-        ];
-    }
-
-    protected function getTableBulkActions(): array
-    {
-        return [
-            TableActions\DeleteBulkAction::make(),
-        ];
-    }
-
-    public function table(Table $table): Table
-    {
-        $tenantModel = static::getModel();
-        $tableName = app($tenantModel)->getTable();
-
-        // Check if the table exists
-        if (! $this->tableExists($tenantModel)) {
-            $this->notifyTableMissing($tableName);
-
-            return $this->configureEmptyTable($table);
-        }
-
-        return $this->configureTable($table);
-    }
-
-    protected function tableExists(string $model): bool
-    {
-        return app($model)->getConnection()->getSchemaBuilder()->hasTable(app($model)->getTable());
-    }
-
-    protected function notifyTableMissing(string $tableName): void
-    {
-        Notification::make()
-            ->title(__('Attenzione'))
-            ->body(__('La tabella :table non esiste', ['table' => $tableName]))
-            ->persistent()
-            ->warning()
-            ->send();
-    }
-
-    protected function configureEmptyTable(Table $table): Table
-    {
-        return $table
-            ->modifyQueryUsing(fn (Builder $query) => User::query()->where('id', null))
-            ->columns([
-                TextColumn::make('message')
-                    ->label(__('Messaggio'))
-                    ->default(__('La tabella tenants non è stata impostata. Per favore, configurala.'))
-                    ->html(), // Allows you to format the message if needed
-            ])
-            ->headerActions([]) // No actions
-            ->actions([]); // No footer actions
-    }
-
-    protected function configureTable(Table $table): Table
-    {
-        return $table
-            ->columns($this->layoutView->getTableColumns())
-            ->contentGrid($this->layoutView->getTableContentGrid())
-            ->headerActions($this->getTableHeaderActions())
-            ->filters($this->getTableFilters())
-            ->filtersLayout(FiltersLayout::AboveContent)
-            ->persistFiltersInSession()
-            ->actions($this->getTableActions())
-            ->bulkActions($this->getTableBulkActions())
-            ->actionsPosition(ActionsPosition::BeforeColumns)
-            ->defaultSort(column: 'created_at', direction: 'DESC');
-    }
-
-    public function getGridTableColumns(): array
-    {
-        return [
-            Stack::make($this->getListTableColumns()),
-        ];
-    }
-
-    public function getListTableColumns(): array
-    {
-        return [
-            TextColumn::make('id')->label(__('ID'))->searchable()->sortable(),
-            TextColumn::make('name')->label(__('Nome')),
-        ];
-    }
-
-    protected function getTableFilters(): array
-    {
-        return [
-            // Define your filters here
-        ];
-    }
-
-    protected function getTableActions(): array
-    {
-        return [
-            TableActions\ViewAction::make(),
-            TableActions\EditAction::make(),
-        ];
-    }
-
-    protected function getTableBulkActions(): array
-    {
-        return [
-            TableActions\DeleteBulkAction::make(),
-        ];
-    }
-
-    public function table(Table $table): Table
-    {
-        $tenantModel = static::getModel();
-
-        // Check if the table exists
-        if (empty($tenantModel) || ! app($tenantModel)->getConnection()->getSchemaBuilder()->hasTable(app($tenantModel)->getTable())) {
-            Notification::make()
-            ->title(__('Attenzione'))
-            ->body(__('La tabella :table non esiste', ['table' => app($tenantModel)->getTable()]))
-            ->persistent()
-            ->warning()
-            ->send();
-
-            return $this->configureEmptyTable($table);
-        }
-
-        return $this->configureTable($table);
-    }
-
-    protected function configureEmptyTable(Table $table): Table
-    {
-        return $table
-            ->modifyQueryUsing(fn (Builder $query) => User::query()->where('id', null))
-            ->columns([
-                TextColumn::make('message')
-                    ->label(__('Messaggio'))
-                    ->default(__('La tabella tenants non è stata impostata. Per favore, configurala.'))
-                    ->html(), // This allows you to format the message if needed
-            ])
-            ->headerActions([]) // No actions
-            ->actions([]); // No footer actions
-    }
-
-    protected function configureTable(Table $table): Table
-    {
-        return $table
-        ->columns($this->layoutView->getTableColumns())
-        ->contentGrid($this->layoutView->getTableContentGrid())
-        ->headerActions($this->getTableHeaderActions())
-
-        ->filters($this->getTableFilters())
-        ->filtersLayout(FiltersLayout::AboveContent)
-        ->persistFiltersInSession()
-        ->actions($this->getTableActions())
-        ->bulkActions($this->getTableBulkActions())
-        ->actionsPosition(ActionsPosition::BeforeColumns)
-        ->defaultSort(
-            column: 'created_at',
-            direction: 'DESC',
-        );
-    }
-
-    public function getGridTableColumns(): array
-    {
-        return [
-            Stack::make($this->getListTableColumns()),
-        ];
-    }
-
-    public function getListTableColumns(): array
-    {
-        return [
-            TextColumn::make('id')->label(__('ID'))->searchable()->sortable(),
-            TextColumn::make('name')->label(__('Nome')),
-        ];
-    }
-
-    protected function getTableFilters(): array
-    {
-        return [
-            // Your filters here
-        ];
-    }
-
-    protected function getTableActions(): array
-    {
-        return [
-            TableActions\ViewAction::make(),
-            TableActions\EditAction::make(),
-        ];
-    }
-
-    protected function getTableBulkActions(): array
-    {
-        return [
-            TableActions\DeleteBulkAction::make(),
-        ];
-    }
 }
