377e58436204f7a8496cd622529f231f360e5e6c
marco76tv <marco.sottana@gmail.com>
2024-10-29 13:39:54 +0100

    up

--- Filament/Resources/TenantResource/RelationManagers/UsersRelationManager.php
+++ Filament/Resources/TenantResource/RelationManagers/UsersRelationManager.php
@@ -4,33 +4,34 @@ declare(strict_types=1);
 
 namespace Modules\User\Filament\Resources\TenantResource\RelationManagers;
 
-use Filament\Actions;
-use Filament\Actions\CreateAction;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\RelationManagers\RelationManager;
+
 use Filament\Tables;
-use Filament\Tables\Actions\AssociateAction;
-use Filament\Tables\Actions\CreateAction;
-use Filament\Tables\Actions\DeleteAction;
-use Filament\Tables\Actions\DeleteBulkAction;
-use Filament\Tables\Actions\DetachAction;
-use Filament\Tables\Actions\DetachBulkAction;
-use Filament\Tables\Actions\EditAction;
-use Filament\Tables\Columns\Layout\Stack;
-use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Enums\ActionsPosition;
-use Filament\Tables\Enums\FiltersLayout;
+use Filament\Actions;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
 use Modules\UI\Enums\TableLayoutEnum;
-use Modules\UI\Filament\Actions\Table\TableLayoutToggleTableAction;
+
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Enums\FiltersLayout;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\DetachAction;
+use Filament\Tables\Columns\Layout\Stack;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Tables\Enums\ActionsPosition;
 use Modules\Xot\Filament\Traits\TransTrait;
+use Filament\Tables\Actions\AssociateAction;
+use Modules\Xot\Filament\Traits\HasXotTable;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Tables\Actions\DetachBulkAction;
+use Filament\Resources\RelationManagers\RelationManager;
+use Modules\UI\Filament\Actions\Table\TableLayoutToggleTableAction;
 
 class UsersRelationManager extends RelationManager
 {
     use TransTrait;
-
+    use HasXotTable;
     protected static string $relationship = 'users';
     protected static ?string $recordTitleAttribute = 'name';
     public TableLayoutEnum $layoutView = TableLayoutEnum::LIST;
@@ -76,31 +77,7 @@ class UsersRelationManager extends RelationManager
         ];
     }
 
-    public function table(Table $table): Table
-    {
-        return $table
-            ->columns($this->layoutView->getTableColumns())
-            ->contentGrid($this->layoutView->getTableContentGrid())
-            ->headerActions($this->getTableHeaderActions())
-            ->filters([
-                Tables\Filters\TrashedFilter::make(),
-                Tables\Filters\Filter::make('verified')
-                    ->label(trans('user::user.filters.verified'))
-                    ->query(fn (Builder $query) => $query->whereNotNull('email_verified_at')),
-                Tables\Filters\Filter::make('unverified')
-                    ->label(trans('user::user.filters.unverified'))
-                    ->query(fn (Builder $query) => $query->whereNull('email_verified_at')),
-            ])
-            ->filtersLayout(FiltersLayout::AboveContent)
-            ->persistFiltersInSession()
-            ->actions($this->getTableActions())
-            ->bulkActions($this->getTableBulkActions())
-            ->actionsPosition(ActionsPosition::BeforeColumns)
-            ->defaultSort('created_at', 'desc')
-            ->reorderable('sort')
-            ->striped()
-            ->paginated([10, 25, 50, 100]);
-    }
+
 
     public function getListTableColumns(): array
     {
@@ -142,82 +119,5 @@ class UsersRelationManager extends RelationManager
         ];
     }
 
-    public function getGridTableColumns(): array
-    {
-        return [
-            Stack::make($this->getListTableColumns()),
-        ];
-    }
 
-    /**
-     * Summary of getTableHeaderActions.
-     *
-     * @return array<\Filament\Tables\Actions\Action|\Filament\Tables\Actions\ActionGroup>
-     */
-    protected function getTableHeaderActions(): array
-    {
-        return [
-            TableLayoutToggleTableAction::make()
-                ->label(trans('user::user.actions.toggle_layout')),
-
-            CreateAction::make()
-                ->label(trans('user::user.actions.create'))
-                ->modalHeading(trans('user::user.modals.create.heading'))
-                ->modalDescription(trans('user::user.modals.create.description'))
-                ->modalSubmitActionLabel(trans('user::user.modals.create.actions.submit'))
-                ->modalCancelActionLabel(trans('user::user.modals.create.actions.cancel')),
-
-            AssociateAction::make()
-                ->label(trans('user::user.actions.associate'))
-                ->modalHeading(trans('user::user.modals.associate.heading'))
-                ->modalDescription(trans('user::user.modals.associate.description'))
-                ->modalSubmitActionLabel(trans('user::user.modals.associate.actions.submit'))
-                ->modalCancelActionLabel(trans('user::user.modals.associate.actions.cancel')),
-        ];
-    }
-
-    protected function getTableActions(): array
-    {
-        return [
-            EditAction::make()
-                ->label(trans('user::user.actions.edit'))
-                ->modalHeading(trans('user::user.modals.edit.heading'))
-                ->modalDescription(trans('user::user.modals.edit.description'))
-                ->modalSubmitActionLabel(trans('user::user.modals.edit.actions.submit'))
-                ->modalCancelActionLabel(trans('user::user.modals.edit.actions.cancel')),
-
-            DeleteAction::make()
-                ->label(trans('user::user.actions.delete'))
-                ->modalHeading(trans('user::user.modals.delete.heading'))
-                ->modalDescription(trans('user::user.modals.delete.description'))
-                ->modalSubmitActionLabel(trans('user::user.modals.delete.actions.submit'))
-                ->modalCancelActionLabel(trans('user::user.modals.delete.actions.cancel')),
-
-            DetachAction::make()
-                ->label(trans('user::user.actions.detach'))
-                ->modalHeading(trans('user::user.modals.detach.heading'))
-                ->modalDescription(trans('user::user.modals.detach.description'))
-                ->modalSubmitActionLabel(trans('user::user.modals.detach.actions.submit'))
-                ->modalCancelActionLabel(trans('user::user.modals.detach.actions.cancel')),
-        ];
-    }
-
-    protected function getTableBulkActions(): array
-    {
-        return [
-            DeleteBulkAction::make()
-                ->label(trans('user::user.actions.bulk_delete'))
-                ->modalHeading(trans('user::user.modals.bulk_delete.heading'))
-                ->modalDescription(trans('user::user.modals.bulk_delete.description'))
-                ->modalSubmitActionLabel(trans('user::user.modals.bulk_delete.actions.submit'))
-                ->modalCancelActionLabel(trans('user::user.modals.bulk_delete.actions.cancel')),
-
-            DetachBulkAction::make()
-                ->label(trans('user::user.actions.bulk_detach'))
-                ->modalHeading(trans('user::user.modals.bulk_detach.heading'))
-                ->modalDescription(trans('user::user.modals.bulk_detach.description'))
-                ->modalSubmitActionLabel(trans('user::user.modals.bulk_detach.actions.submit'))
-                ->modalCancelActionLabel(trans('user::user.modals.bulk_detach.actions.cancel')),
-        ];
-    }
 }
