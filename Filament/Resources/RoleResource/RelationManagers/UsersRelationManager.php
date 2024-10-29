334918b298b320b3383726f3255a97e1ee371ee4
marco <marco.sottana@gmail.com>
2024-10-29 15:39:12 +0100

    .

--- Filament/Resources/RoleResource/RelationManagers/UsersRelationManager.php
+++ Filament/Resources/RoleResource/RelationManagers/UsersRelationManager.php
@@ -7,24 +7,17 @@ namespace Modules\User\Filament\Resources\RoleResource\RelationManagers;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
-use Filament\Tables;
-use Filament\Tables\Actions\DeleteBulkAction;
-use Filament\Tables\Actions\EditAction;
-use Filament\Tables\Actions\ViewAction;
-use Filament\Tables\Columns\Layout\Stack;
 use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Enums\ActionsPosition;
-use Filament\Tables\Enums\FiltersLayout;
 use Filament\Tables\Filters\Filter;
-use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Modules\UI\Enums\TableLayoutEnum;
-use Modules\UI\Filament\Actions\Table\TableLayoutToggleTableAction;
+use Modules\Xot\Filament\Traits\HasXotTable;
 use Modules\Xot\Filament\Traits\TransTrait;
 
 final class UsersRelationManager extends RelationManager
 {
     use TransTrait;
+    use HasXotTable;
 
     protected static string $relationship = 'users';
     protected static ?string $inverseRelationship = 'roles';
@@ -49,38 +42,6 @@ final class UsersRelationManager extends RelationManager
                 ->required()
                 ->maxLength(255),
             // Additional fields as needed
-        ];
-    }
-
-    /**
-     * Configure the table structure and behavior.
-     */
-    public function table(Table $table): Table
-    {
-        return $table
-            ->columns($this->layoutView->getTableColumns())
-            ->contentGrid($this->layoutView->getTableContentGrid())
-            ->headerActions($this->getTableHeaderActions())
-            ->filters($this->getTableFilters())
-            ->filtersLayout(FiltersLayout::AboveContent)
-            ->filtersFormColumns(3)
-            ->persistFiltersInSession()
-            ->actions($this->getTableActions())
-            ->bulkActions($this->getTableBulkActions())
-            ->actionsPosition(ActionsPosition::BeforeColumns)
-            ->defaultSort('users.created_at', 'desc')
-            ->striped()
-            ->paginated([10, 25, 50, 100])
-            ->poll('60s');
-    }
-
-    /**
-     * Get grid layout columns.
-     */
-    public function getGridTableColumns(): array
-    {
-        return [
-            Stack::make($this->getListTableColumns()),
         ];
     }
 
@@ -117,66 +78,6 @@ final class UsersRelationManager extends RelationManager
     }
 
     /**
-     * Get header actions.
-     */
-    protected function getTableHeaderActions(): array
-    {
-        return [
-            TableLayoutToggleTableAction::make(),
-            Tables\Actions\AssociateAction::make()
-                ->label('')
-                ->icon('heroicon-o-link')
-                ->tooltip(__('user::actions.associate_user')),
-            Tables\Actions\AttachAction::make()
-                ->label('')
-                ->icon('heroicon-o-paper-clip')
-                ->tooltip(__('user::actions.attach_user')),
-        ];
-    }
-
-    /**
-     * Get row-level actions.
-     */
-    protected function getTableActions(): array
-    {
-        return [
-            ViewAction::make()
-                ->label('')
-                ->tooltip(__('user::actions.view'))
-                ->icon('heroicon-o-eye')
-                ->color('info'),
-
-            EditAction::make()
-                ->label('')
-                ->tooltip(__('user::actions.edit'))
-                ->icon('heroicon-o-pencil')
-                ->color('warning'),
-
-            Tables\Actions\DetachAction::make()
-                ->label('')
-                ->tooltip(__('user::actions.detach'))
-                ->icon('heroicon-o-link-slash')
-                ->color('danger')
-                ->requiresConfirmation(),
-        ];
-    }
-
-    /**
-     * Get bulk actions.
-     */
-    protected function getTableBulkActions(): array
-    {
-        return [
-            DeleteBulkAction::make()
-                ->label('')
-                ->tooltip(__('user::actions.delete_selected'))
-                ->icon('heroicon-o-trash')
-                ->color('danger')
-                ->requiresConfirmation(),
-        ];
-    }
-
-    /**
      * Get filters.
      */
     protected function getTableFilters(): array
@@ -199,60 +100,6 @@ final class UsersRelationManager extends RelationManager
                         ->when($data['created_until'], fn (Builder $query, $date) => $query->whereDate('created_at', '<=', $date));
                 })
                 ->columns(2),
-        ];
-    }
-
-    // public function getTableFilters(): array
-    // {
-    //     return [
-    //     ];
-    // }
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            Tables\Actions\CreateAction::make()
-                ->label('') // Empty label
-                ->tooltip(__('Create User')), // Move label to tooltip
-            Tables\Actions\AssociateAction::make()
-                ->label('') // Empty label
-                ->tooltip(__('Associate User')), // Move label to tooltip
-        ];
-    }
-
-    // protected function getTableHeaderActions(): array
-    // {
-    //     return [
-    //         TableLayoutToggleTableAction::make(),
-    //         Tables\Actions\AssociateAction::make()
-    //             ->label('') // Empty label
-    //             ->icon('heroicon-o-link')
-    //             ->tooltip(__('Associate User')), // Move label to tooltip
-    //         Tables\Actions\AttachAction::make()
-    //             ->label('') // Empty label
-    //             ->icon('heroicon-o-paper-clip')
-    //             ->tooltip(__('Attach User')), // Move label to tooltip
-    //     ];
-    // }
-
-    // protected function getTableActions(): array
-    // {
-    //     return [
-    //         EditAction::make()
-    //             ->label('') // Empty label
-    //             ->tooltip(__('Edit')), // Move label to tooltip
-    //         DeleteAction::make()
-    //             ->label('') // Empty label
-    //             ->tooltip(__('Delete')), // Move label to tooltip
-    //     ];
-    // }
-
-    protected function getBulkActions(): array
-    {
-        return [
-            DeleteBulkAction::make()
-                ->label('') // Empty label
-                ->tooltip(__('Delete Selected')), // Move label to tooltip
         ];
     }
 }
