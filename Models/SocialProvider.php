1402092ffda2ce219f9463c5e957ec5d31df08ed
marco <marco.sottana@gmail.com>
2024-10-29 17:35:04 +0100

    up

--- Models/SocialProvider.php
+++ Models/SocialProvider.php
@@ -95,7 +95,7 @@ class SocialProvider extends BaseModel
     }
 
     /** @return array<string, string> */
-    public function casts(): array
+    protected function casts(): array
     {
         return [
             'scopes' => 'array',
--- Models/TenantUser.php
+++ Models/TenantUser.php
@@ -61,7 +61,7 @@ class TenantUser extends BasePivot
     ];
 
     /** @return array<string, string> */
-    public function casts(): array
+    protected function casts(): array
     {
         return [
             'id' => 'string',
