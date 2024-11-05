5a473aa843f22015cb2c7e846f828525ca501568
marco76tv <marco.sottana@gmail.com>
2024-11-05 09:40:55 +0100

    🔧 (UsersRelationManager.php): Remove unnecessary code related to Filament Tables and Actions

--- Filament/Resources/RoleResource/RelationManagers/UsersRelationManager.php
+++ Filament/Resources/RoleResource/RelationManagers/UsersRelationManager.php
@@ -7,23 +7,11 @@ namespace Modules\User\Filament\Resources\RoleResource\RelationManagers;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
-<<<<<<< HEAD
-=======
-use Filament\Tables\Actions\AttachAction;
-use Filament\Tables\Actions\DetachAction;
-use Filament\Tables\Actions\EditAction;
-use Filament\Tables\Actions\ViewAction;
-use Filament\Tables\Columns\Layout\Stack;
->>>>>>> 3b22a638 (🔧 (UsersRelationManager.php): Remove unnecessary code related to Filament Tables and Actions)
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\Filter;
 use Illuminate\Database\Eloquent\Builder;
 use Modules\UI\Enums\TableLayoutEnum;
-<<<<<<< HEAD
 use Modules\Xot\Filament\Traits\HasXotTable;
-=======
-use Modules\User\Filament\Resources\UserResource;
->>>>>>> 3b22a638 (🔧 (UsersRelationManager.php): Remove unnecessary code related to Filament Tables and Actions)
 use Modules\Xot\Filament\Traits\TransTrait;
 
 final class UsersRelationManager extends RelationManager
@@ -36,31 +24,17 @@ final class UsersRelationManager extends RelationManager
     protected static ?string $recordTitleAttribute = 'name';
     public TableLayoutEnum $layoutView = TableLayoutEnum::LIST;
 
-<<<<<<< HEAD
-=======
-    protected static string $relationship = 'users';
-
-    protected static ?string $inverseRelationship = 'roles';
-
-    protected static ?string $recordTitleAttribute = 'name';
-
->>>>>>> 3b22a638 (🔧 (UsersRelationManager.php): Remove unnecessary code related to Filament Tables and Actions)
     /**
      * Definisce lo schema del form per la relazione users.
      */
     public function form(Form $form): Form
     {
-<<<<<<< HEAD
         return $form->schema($this->getFormSchema());
-=======
-        return $this->getUserResourceForm($form);
->>>>>>> 3b22a638 (🔧 (UsersRelationManager.php): Remove unnecessary code related to Filament Tables and Actions)
     }
 
     /**
      * Restituisce lo schema del form.
      */
-<<<<<<< HEAD
     protected function getFormSchema(): array
     {
         return [
@@ -69,38 +43,6 @@ final class UsersRelationManager extends RelationManager
                 ->maxLength(255)
                 ->label(__('user::socialite_user.fields.name.label')),
             // Aggiungi altri campi necessari
-=======
-    protected function getUserResourceForm(Form $form): Form
-    {
-        // Centralize form structure using UserResource for consistency
-        return UserResource::form($form);
-    }
-
-    public function table(Table $table): Table
-    {
-        return $table
-            // ->columns($this->getTableColumns())
-            ->columns($this->layoutView->getTableColumns())
-            ->contentGrid($this->layoutView->getTableContentGrid())
-            ->headerActions($this->getTableHeaderActions())
-
-            ->filters($this->getTableFilters())
-            ->filtersLayout(FiltersLayout::AboveContent)
-            ->persistFiltersInSession()
-            ->actions($this->getTableActions())
-            ->bulkActions($this->getTableBulkActions())
-            ->actionsPosition(ActionsPosition::BeforeColumns)
-            ->defaultSort(
-                column: 'users.created_at',
-                direction: 'DESC',
-            );
-    }
-
-    public function getGridTableColumns(): array
-    {
-        return [
-            Stack::make($this->getListTableColumns()),
->>>>>>> 3b22a638 (🔧 (UsersRelationManager.php): Remove unnecessary code related to Filament Tables and Actions)
         ];
     }
 
@@ -147,7 +89,6 @@ final class UsersRelationManager extends RelationManager
                 ->query(fn (Builder $query): Builder => $query->where('is_active', true))
                 ->toggle(),
 
-<<<<<<< HEAD
             Filter::make('created_at')
                 ->label(__('user::filters.creation_date'))
                 ->form([
@@ -160,80 +101,6 @@ final class UsersRelationManager extends RelationManager
                         ->when($data['created_until'], fn (Builder $query, $date) => $query->whereDate('created_at', '<=', $date));
                 })
                 ->columns(2),
-=======
-    /**
-     * Define the row-level actions in a separate function.
-     */
-    protected function getTableActions(): array
-    {
-        return [
-            ViewAction::make()
-                ->label('')
-                ->tooltip(__('role.view_user'))
-                ->icon('heroicon-o-eye'),
-            EditAction::make()
-                ->label('')
-                ->tooltip(__('role.edit_user'))
-                ->icon('heroicon-o-pencil'),
-            DetachAction::make()
-                ->label('')
-                ->tooltip(__('role.detach_user'))
-                ->icon('heroicon-o-link-slash'),
-        ];
-    }
-
-    public function getTableFilters(): array
-    {
-        return [
-        ];
-    }
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            Tables\Actions\CreateAction::make()
-                ->label('') // Empty label
-                ->tooltip(__('Create User')), // Move label to tooltip
-            Tables\Actions\AssociateAction::make()
-                ->label('') // Empty label
-                ->tooltip(__('Associate User')), // Move label to tooltip
-        ];
-    }
-
-    protected function getTableHeaderActions(): array
-    {
-        return [
-            TableLayoutToggleTableAction::make(),
-            Tables\Actions\AssociateAction::make()
-                ->label('') // Empty label
-                ->icon('heroicon-o-link')
-                ->tooltip(__('Associate User')), // Move label to tooltip
-            Tables\Actions\AttachAction::make()
-                ->label('') // Empty label
-                ->icon('heroicon-o-paper-clip')
-                ->tooltip(__('Attach User')), // Move label to tooltip
-        ];
-    }
-
-    protected function getTableActions(): array
-    {
-        return [
-            EditAction::make()
-                ->label('') // Empty label
-                ->tooltip(__('Edit')), // Move label to tooltip
-            DeleteAction::make()
-                ->label('') // Empty label
-                ->tooltip(__('Delete')), // Move label to tooltip
-        ];
-    }
-
-    protected function getBulkActions(): array
-    {
-        return [
-            DeleteBulkAction::make()
-                ->label('') // Empty label
-                ->tooltip(__('Delete Selected')), // Move label to tooltip
->>>>>>> 3b22a638 (🔧 (UsersRelationManager.php): Remove unnecessary code related to Filament Tables and Actions)
         ];
     }
 }
