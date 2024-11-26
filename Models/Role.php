c9decd8cd0116100d86c5abab319e7a0b67905ac
marco76tv <marco76tv@users.noreply.github.com>
2024-11-26 07:43:53 +0000

    Check & fix styling

--- Models/Role.php
+++ Models/Role.php
@@ -25,18 +25,18 @@ use Webmozart\Assert\Assert;
 /**
  * Modules\User\Models\Role.
  *
- * @property string                                                                    $id
- * @property string                                                                    $uuid
- * @property string|null                                                               $team_id
- * @property string                                                                    $name
- * @property string                                                                    $guard_name
- * @property Carbon|null                                                               $created_at
- * @property Carbon|null                                                               $updated_at
- * @property Collection<int, \Modules\User\Models\Permission>                          $permissions
- * @property int|null                                                                  $permissions_count
- * @property Team|null                                                                 $team
- * @property EloquentCollection<int, \Illuminate\Database\Eloquent\Model&UserContract> $users
- * @property int|null                                                                  $users_count
+ * @property string                                      $id
+ * @property string                                      $uuid
+ * @property string|null                                 $team_id
+ * @property string                                      $name
+ * @property string                                      $guard_name
+ * @property Carbon|null                                 $created_at
+ * @property Carbon|null                                 $updated_at
+ * @property Collection<int, Permission>                 $permissions
+ * @property int|null                                    $permissions_count
+ * @property Team|null                                   $team
+ * @property EloquentCollection<int, Model&UserContract> $users
+ * @property int|null                                    $users_count
  *
  * @method static \Illuminate\Database\Eloquent\Builder|Role newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Role newQuery()
