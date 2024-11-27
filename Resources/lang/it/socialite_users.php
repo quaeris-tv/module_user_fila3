e89ecaf1569f4b281270c897db9f7a8108fcd2b9
marco76tv <marco.sottana@gmail.com>
2024-11-27 17:55:53 +0100

    up

---
+++ Resources/lang/it/socialite_users.php
@@ -0,0 +1,25 @@
+<?php return array (
+  'fields' => 
+  array (
+    'provider' => 
+    array (
+      'label' => 'provider',
+    ),
+    'provider_id' => 
+    array (
+      'label' => 'provider_id',
+    ),
+    'name' => 
+    array (
+      'label' => 'name',
+    ),
+    'email' => 
+    array (
+      'label' => 'email',
+    ),
+    'avatar' => 
+    array (
+      'label' => 'avatar',
+    ),
+  ),
+);
