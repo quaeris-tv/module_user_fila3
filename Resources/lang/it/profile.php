08cc6b52596a1a6a3b68a9daca89c31d1c2b5bd8
marco76tv <marco.sottana@gmail.com>
2024-11-28 16:00:13 +0100

    up

--- Resources/lang/it/profile.php
+++ Resources/lang/it/profile.php
@@ -1,46 +1,82 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    'account' => 'Account',
-    'profile' => 'Profilo',
-    'my_profile' => 'Il mio profilo',
-    'subheading' => 'Gestisci il tuo profilo.',
-    'fields' => [
-        'type' => [
-            'label' => 'Tipo',
-        ],
-        'user' => [
-            'name' => [
-                'label' => 'Nome Utente',
-            ],
-        ],
-        'photo' => [
-            'label' => 'Foto',
-        ],
-        'id' => [
-            'label' => 'id',
-        ],
-        'ente' => [
-            'label' => 'ente',
-        ],
-        'matr' => [
-            'label' => 'matr',
-        ],
-        'first_name' => [
-            'label' => 'first_name',
-        ],
-        'last_name' => [
-            'label' => 'last_name',
-        ],
-    ],
-    'personal_info' => [
-        'heading' => 'Informazioni personali',
-        'subheading' => 'Gestisci le tue informazioni personali.',
-        'submit' => [
-            'label' => 'Aggiorna',
-        ],
-        'notify' => 'Profilo aggiornato correttamente!',
-    ],
-];
+<?php return array (
+  'account' => 'Account',
+  'profile' => 'Profilo',
+  'my_profile' => 'Il mio profilo',
+  'subheading' => 'Gestisci il tuo profilo.',
+  'fields' => 
+  array (
+    'type' => 
+    array (
+      'label' => 'Tipo',
+      'fields' => 
+      array (
+        'label' => 'Tipo',
+      ),
+    ),
+    'user' => 
+    array (
+      'name' => 
+      array (
+        'label' => 'Nome Utente',
+        'fields' => 
+        array (
+          'label' => 'Nome Utente',
+        ),
+      ),
+    ),
+    'photo' => 
+    array (
+      'label' => 'Foto',
+      'fields' => 
+      array (
+        'label' => 'Foto',
+      ),
+    ),
+    'id' => 
+    array (
+      'label' => 'id',
+    ),
+    'ente' => 
+    array (
+      'label' => 'ente',
+    ),
+    'matr' => 
+    array (
+      'label' => 'matr',
+    ),
+    'first_name' => 
+    array (
+      'label' => 'first_name',
+      'fields' => 
+      array (
+        'label' => 'first_name',
+      ),
+    ),
+    'last_name' => 
+    array (
+      'label' => 'last_name',
+      'fields' => 
+      array (
+        'label' => 'last_name',
+      ),
+    ),
+    'email' => 
+    array (
+      'fields' => 'email',
+    ),
+    'is_active' => 
+    array (
+      'fields' => 'is_active',
+    ),
+  ),
+  'personal_info' => 
+  array (
+    'heading' => 'Informazioni personali',
+    'subheading' => 'Gestisci le tue informazioni personali.',
+    'submit' => 
+    array (
+      'label' => 'Aggiorna',
+    ),
+    'notify' => 'Profilo aggiornato correttamente!',
+  ),
+);
