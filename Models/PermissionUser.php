6046e2e0641adacfedd97568d49ab042d6591fe6
marco76tv <marco.sottana@gmail.com>
2024-12-04 13:27:19 +0100

    up

---
+++ Models/PermissionUser.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\User\Models;
+
+
+/**
+ * 
+ *
+ * @property-read \Modules\Blog\Models\Profile|null $creator
+ * @property-read \Modules\Blog\Models\Profile|null $updater
+ * @method static \Modules\User\Database\Factories\PermissionUserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|PermissionUser newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|PermissionUser newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|PermissionUser query()
+ * @mixin \Eloquent
+ */
+class PermissionUser extends ModelHasPermission
+{
+
+}
