26e6184cbbfa8e5a76ba647e49eaf03e74dd9eef
marco76tv <marco.sottana@gmail.com>
2024-12-05 12:57:58 +0100

    up

--- Filament/Resources/BaseProfileResource/Pages/ListProfiles.php
+++ Filament/Resources/BaseProfileResource/Pages/ListProfiles.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Modules\User\Filament\Resources\BaseProfileResource\Pages;
 
-use Filament\Actions;
 use Filament\Tables;
 use Filament\Tables\Actions\BulkAction;
 use Filament\Tables\Columns\IconColumn;
@@ -12,12 +11,10 @@ use Filament\Tables\Columns\Layout\Stack;
 use Filament\Tables\Columns\SpatieMediaLibraryImageColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\TernaryFilter;
-use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
-use Modules\UI\Filament\Actions\Table\TableLayoutToggleTableAction;
 use Modules\User\Filament\Actions\Profile\ChangeProfilePasswordAction;
 use Modules\User\Filament\Resources\BaseProfileResource;
 use Modules\Xot\Datas\XotData;
@@ -42,9 +39,8 @@ class ListProfiles extends XotBaseListRecords
     {
         return [
             Stack::make([
-                'type' => TextColumn::make('type')
-
-                    ->sortable(),
+                // 'type' => TextColumn::make('type')
+                //    ->sortable(),
 
                 'user_name' => TextColumn::make('user.name')
 
@@ -93,12 +89,10 @@ class ListProfiles extends XotBaseListRecords
     public function getListTableColumns(): array
     {
         return [
-            'type' => TextColumn::make('type')
-
-                ->sortable(),
+            // 'type' => TextColumn::make('type')
+            //    ->sortable(),
 
             'user_name' => TextColumn::make('user.name')
-
                 ->sortable()
                 ->searchable()
                 ->default(
@@ -140,81 +134,11 @@ class ListProfiles extends XotBaseListRecords
         ];
     }
 
-    public function table(Table $table): Table
-    {
-        return $table
-            // ->query($this->getTableQuery())
-            ->actions($this->getTableActions())
-            // ->actionsColumnLabel($this->getTableActionsColumnLabel())
-            // ->checkIfRecordIsSelectableUsing($this->isTableRecordSelectable())
-
-            // ->columns($this->getTableColumns())
-            ->columns($this->layoutView->getTableColumns())
-            ->contentGrid($this->layoutView->getTableContentGrid())
-            ->headerActions($this->getTableHeaderActions())
-
-            // ->columnToggleFormColumns($this->getTableColumnToggleFormColumns())
-            // ->columnToggleFormMaxHeight($this->getTableColumnToggleFormMaxHeight())
-            // ->columnToggleFormWidth($this->getTableColumnToggleFormWidth())
-            // ->content($this->getTableContent())
-            // ->contentFooter($this->getTableContentFooter())
-            // ->contentGrid($this->getTableContentGrid())
-            // ->defaultSort($this->getDefaultTableSortColumn(), $this->getDefaultTableSortDirection())
-            // ->deferLoading($this->isTableLoadingDeferred())
-            // ->description($this->getTableDescription())
-            // ->deselectAllRecordsWhenFiltered($this->shouldDeselectAllRecordsWhenTableFiltered())
-            // ->emptyState($this->getTableEmptyState())
-            // ->emptyStateActions($this->getTableEmptyStateActions())
-            // ->emptyStateDescription($this->getTableEmptyStateDescription())
-            // ->emptyStateHeading($this->getTableEmptyStateHeading())
-            // ->emptyStateIcon($this->getTableEmptyStateIcon())
-            ->filters($this->getTableFilters())
-            // ->filtersFormMaxHeight($this->getTableFiltersFormMaxHeight())
-            // ->filtersFormWidth($this->getTableFiltersFormWidth())
-            // ->groupedBulkActions($this->getTableBulkActions())
-            ->bulkActions($this->getTableBulkActions());
-        // ->header($this->getTableHeader())
-        // ->headerActions($this->getTableHeaderActions())
-        // ->modelLabel($this->getTableModelLabel())
-        // ->paginated($this->isTablePaginationEnabled())
-        // ->paginatedWhileReordering($this->isTablePaginationEnabledWhileReordering())
-        // ->paginationPageOptions($this->getTableRecordsPerPageSelectOptions())
-        // ->persistFiltersInSession($this->shouldPersistTableFiltersInSession())
-        // ->persistSearchInSession($this->shouldPersistTableSearchInSession())
-        // ->persistColumnSearchesInSession($this->shouldPersistTableColumnSearchInSession())
-        // ->persistSortInSession($this->shouldPersistTableSortInSession())
-        // ->pluralModelLabel($this->getTablePluralModelLabel())
-        // ->poll($this->getTablePollingInterval())
-        // ->recordAction($this->getTableRecordActionUsing())
-        // ->recordClasses($this->getTableRecordClassesUsing())
-        // ->recordTitle(fn (Model $record): ?string => $this->getTableRecordTitle($record))
-        // ->recordUrl($this->getTableRecordUrlUsing())
-        // ->reorderable($this->getTableReorderColumn())
-        // ->selectCurrentPageOnly($this->shouldSelectCurrentPageOnly())
-        // ->striped($this->isTableStriped())
-    }
-
-    protected function getTableHeaderActions(): array
-    {
-        return [
-            TableLayoutToggleTableAction::make(),
-        ];
-    }
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\CreateAction::make(),
-        ];
-    }
-
     protected function getTableActions(): array
     {
         return [
             ChangeProfilePasswordAction::make(),
-            Tables\Actions\EditAction::make()->label('')->tooltip(__('ui::txt.edit')),
-            Tables\Actions\ViewAction::make()->label('')->tooltip(__('ui::txt.view')),
-            Tables\Actions\DeleteAction::make()->label('')->tooltip(__('ui::txt.delete')),
+            ...parent::getTableActions(),
         ];
     }
 
--- Resources/lang/it/profile.php
+++ Resources/lang/it/profile.php
@@ -1,50 +1,43 @@
-<?php return array (
-  'account' => 'Account',
-  'profile' => 'Profilo',
-  'my_profile' => 'Il mio profilo',
-  'subheading' => 'Gestisci il tuo profilo.',
-  'personal_info' => 
-  array (
-    'heading' => 'Informazioni personali',
-    'subheading' => 'Gestisci le tue informazioni personali.',
-    'submit' => 
-    array (
-      'label' => 'Aggiorna',
-    ),
-    'notify' => 'Profilo aggiornato correttamente!',
-  ),
-  'fields' => 
-  array (
-    'type' => 
-    array (
-      'label' => 'type',
-    ),
-    'user' => 
-    array (
-      'name' => 
-      array (
-        'label' => 'user.name',
-      ),
-    ),
-    'first_name' => 
-    array (
-      'label' => 'first_name',
-    ),
-    'last_name' => 
-    array (
-      'label' => 'last_name',
-    ),
-    'email' => 
-    array (
-      'label' => 'email',
-    ),
-    'is_active' => 
-    array (
-      'label' => 'is_active',
-    ),
-    'photo' => 
-    array (
-      'label' => 'photo',
-    ),
-  ),
-);
+<?php
+
+declare(strict_types=1);
+
+return [
+    'account' => 'Account',
+    'profile' => 'Profilo',
+    'my_profile' => 'Il mio profilo',
+    'subheading' => 'Gestisci il tuo profilo.',
+    'personal_info' => [
+        'heading' => 'Informazioni personali',
+        'subheading' => 'Gestisci le tue informazioni personali.',
+        'submit' => [
+            'label' => 'Aggiorna',
+        ],
+        'notify' => 'Profilo aggiornato correttamente!',
+    ],
+    'fields' => [
+        'type' => [
+            'label' => 'tipo',
+        ],
+        'user' => [
+            'name' => [
+                'label' => 'Nome Utente',
+            ],
+        ],
+        'first_name' => [
+            'label' => 'Nome',
+        ],
+        'last_name' => [
+            'label' => 'Cognome',
+        ],
+        'email' => [
+            'label' => 'Email',
+        ],
+        'is_active' => [
+            'label' => 'Attivo ?',
+        ],
+        'photo' => [
+            'label' => 'foto',
+        ],
+    ],
+];
