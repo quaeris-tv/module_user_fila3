c72f45098cba78bc61d19c011b57155a29ca054e
marco76tv <marco.sottana@gmail.com>
2024-12-05 11:32:25 +0100

    up

--- Resources/lang/it/profile.php
+++ Resources/lang/it/profile.php
@@ -1,18 +1,50 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    'account' => 'Account',
-    'profile' => 'Profilo',
-    'my_profile' => 'Il mio profilo',
-    'subheading' => 'Gestisci il tuo profilo.',
-    'personal_info' => [
-        'heading' => 'Informazioni personali',
-        'subheading' => 'Gestisci le tue informazioni personali.',
-        'submit' => [
-            'label' => 'Aggiorna',
-        ],
-        'notify' => 'Profilo aggiornato correttamente!',
-    ],
-];
+<?php return array (
+  'account' => 'Account',
+  'profile' => 'Profilo',
+  'my_profile' => 'Il mio profilo',
+  'subheading' => 'Gestisci il tuo profilo.',
+  'personal_info' => 
+  array (
+    'heading' => 'Informazioni personali',
+    'subheading' => 'Gestisci le tue informazioni personali.',
+    'submit' => 
+    array (
+      'label' => 'Aggiorna',
+    ),
+    'notify' => 'Profilo aggiornato correttamente!',
+  ),
+  'fields' => 
+  array (
+    'type' => 
+    array (
+      'label' => 'type',
+    ),
+    'user' => 
+    array (
+      'name' => 
+      array (
+        'label' => 'user.name',
+      ),
+    ),
+    'first_name' => 
+    array (
+      'label' => 'first_name',
+    ),
+    'last_name' => 
+    array (
+      'label' => 'last_name',
+    ),
+    'email' => 
+    array (
+      'label' => 'email',
+    ),
+    'is_active' => 
+    array (
+      'label' => 'is_active',
+    ),
+    'photo' => 
+    array (
+      'label' => 'photo',
+    ),
+  ),
+);
