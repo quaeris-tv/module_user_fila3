b68c68374b6c9017ceb770451fac05028628a3bd
Malebestia <vair81@gmail.com>
2024-12-06 15:43:19 +0100

    rebase 4/16

--- Resources/lang/it/profile.php
+++ Resources/lang/it/profile.php
@@ -13,14 +13,8 @@ return [
             'name' => ['label' => 'Nome Utente'],
         ],
         'photo' => ['label' => 'Foto'],
-        'id' => ['label' => 'id'],
-        'ente' => ['label' => 'ente'],
-        'matr' => ['label' => 'matr'],
-        'first_name' => ['label' => 'Nome'],
-        'last_name' => ['label' => 'Cognome'],
-        'email' => ['label' => 'email'],
-        'is_active' => ['label' => 'Attivo ?'],
     ],
+
     'personal_info' => [
         'heading' => 'Informazioni personali',
         'subheading' => 'Gestisci le tue informazioni personali.',
@@ -28,5 +22,30 @@ return [
             'label' => 'Aggiorna',
         ],
         'notify' => 'Profilo aggiornato correttamente!',
+    ],
+    'fields' => [
+        'type' => [
+            'label' => 'tipo',
+        ],
+        'user' => [
+            'name' => [
+                'label' => 'Nome Utente',
+            ],
+        ],
+        'first_name' => [
+            'label' => 'Nome',
+        ],
+        'last_name' => [
+            'label' => 'Cognome',
+        ],
+        'email' => [
+            'label' => 'Email',
+        ],
+        'is_active' => [
+            'label' => 'Attivo ?',
+        ],
+        'photo' => [
+            'label' => 'foto',
+        ],
     ],
 ];
