b3de2fca8580b2c0bf9aa31437adca45a7b17abd
Malebestia <vair81@gmail.com>
2024-12-09 11:11:20 +0100

    rebase 1/18

--- Resources/lang/it/profile.php
+++ Resources/lang/it/profile.php
@@ -1,43 +1,83 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    'account' => 'Account',
-    'profile' => 'Profilo',
-    'my_profile' => 'Il mio profilo',
-    'subheading' => 'Gestisci il tuo profilo.',
-    'personal_info' => [
-        'heading' => 'Informazioni personali',
-        'subheading' => 'Gestisci le tue informazioni personali.',
-        'submit' => [
-            'label' => 'Aggiorna',
-        ],
-        'notify' => 'Profilo aggiornato correttamente!',
-    ],
-    'fields' => [
-        'type' => [
-            'label' => 'tipo',
-        ],
-        'user' => [
-            'name' => [
-                'label' => 'Nome Utente',
-            ],
-        ],
-        'first_name' => [
-            'label' => 'Nome',
-        ],
-        'last_name' => [
-            'label' => 'Cognome',
-        ],
-        'email' => [
-            'label' => 'Email',
-        ],
-        'is_active' => [
-            'label' => 'Attivo ?',
-        ],
-        'photo' => [
-            'label' => 'foto',
-        ],
-    ],
-];
+<?php 
+return array (
+  'account' => 'Account',
+  'profile' => 'Profilo',
+  'my_profile' => 'Il mio profilo',
+  'subheading' => 'Gestisci il tuo profilo.',
+  'fields' => 
+  array (
+    'type' => 
+    array (
+      'label' => 'Tipo',
+      'fields' => 
+      array (
+        'label' => 'Tipo',
+      ),
+    ),
+    'user' => 
+    array (
+      'name' => 
+      array (
+        'label' => 'Nome Utente',
+        'fields' => 
+        array (
+          'label' => 'Nome Utente',
+        ),
+      ),
+    ),
+    'photo' => 
+    array (
+      'label' => 'Foto',
+      'fields' => 
+      array (
+        'label' => 'Foto',
+      ),
+    ),
+    'id' => 
+    array (
+      'label' => 'id',
+    ),
+    'ente' => 
+    array (
+      'label' => 'ente',
+    ),
+    'matr' => 
+    array (
+      'label' => 'matr',
+    ),
+    'first_name' => 
+    array (
+      'label' => 'first_name',
+      'fields' => 
+      array (
+        'label' => 'first_name',
+      ),
+    ),
+    'last_name' => 
+    array (
+      'label' => 'last_name',
+      'fields' => 
+      array (
+        'label' => 'last_name',
+      ),
+    ),
+    'email' => 
+    array (
+      'fields' => 'email',
+    ),
+    'is_active' => 
+    array (
+      'fields' => 'is_active',
+    ),
+  ),
+  'personal_info' => 
+  array (
+    'heading' => 'Informazioni personali',
+    'subheading' => 'Gestisci le tue informazioni personali.',
+    'submit' => 
+    array (
+      'label' => 'Aggiorna',
+    ),
+    'notify' => 'Profilo aggiornato correttamente!',
+  ),
+);
