887099655673b5c88dbb6c86ef136b6700954a17
marco76tv <marco76tv@users.noreply.github.com>
2024-12-27 10:32:10 +0000

    Check & fix styling

--- lang/it/recent_logins.php
+++ lang/it/recent_logins.php
@@ -1,24 +1,22 @@
-<?php return array (
-  'fields' => 
-  array (
-    'authenticatable' => 
-    array (
-      'name' => 
-      array (
-        'label' => 'authenticatable.name',
-      ),
-    ),
-    'login_at' => 
-    array (
-      'label' => 'login_at',
-    ),
-    'ip_address' => 
-    array (
-      'label' => 'ip_address',
-    ),
-    'user_agent' => 
-    array (
-      'label' => 'user_agent',
-    ),
-  ),
-);
+<?php
+
+declare(strict_types=1);
+
+return [
+    'fields' => [
+        'authenticatable' => [
+            'name' => [
+                'label' => 'authenticatable.name',
+            ],
+        ],
+        'login_at' => [
+            'label' => 'login_at',
+        ],
+        'ip_address' => [
+            'label' => 'ip_address',
+        ],
+        'user_agent' => [
+            'label' => 'user_agent',
+        ],
+    ],
+];
