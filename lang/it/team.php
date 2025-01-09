35dade7ad6f4dd2efa7a3eaca641ca1f80a29041
marco76tv <marco.sottana@gmail.com>
2025-01-09 09:03:07 +0100

    up

--- lang/it/team.php
+++ lang/it/team.php
@@ -1,34 +1,40 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    'navigation' => [
-        'name' => 'Team',
-        'plural' => 'Teams',
-        'group' => [
-            'name' => 'Admin',
-        ],
-    ],
-    'fields' => [
-        'first_name' => 'Nome',
-        'last_name' => 'Cognome',
-    ],
-    'actions' => [
-        'import' => [
-            'fields' => [
-                'import_file' => 'Seleziona un file XLS o CSV da caricare',
-            ],
-        ],
-        'export' => [
-            'filename_prefix' => 'Aree al',
-            'columns' => [
-                'name' => 'Nome area',
-                'parent_name' => 'Nome area livello superiore',
-            ],
-        ],
-        'create' => [
-            'label' => 'create',
-        ],
-    ],
-];
+<?php return array (
+  'navigation' => 
+  array (
+    'name' => 'Team',
+    'plural' => 'Teams',
+    'group' => 
+    array (
+      'name' => 'Admin',
+    ),
+    'label' => 'team.navigation',
+  ),
+  'fields' => 
+  array (
+    'first_name' => 'Nome',
+    'last_name' => 'Cognome',
+  ),
+  'actions' => 
+  array (
+    'import' => 
+    array (
+      'fields' => 
+      array (
+        'import_file' => 'Seleziona un file XLS o CSV da caricare',
+      ),
+    ),
+    'export' => 
+    array (
+      'filename_prefix' => 'Aree al',
+      'columns' => 
+      array (
+        'name' => 'Nome area',
+        'parent_name' => 'Nome area livello superiore',
+      ),
+    ),
+    'create' => 
+    array (
+      'label' => 'create',
+    ),
+  ),
+);
