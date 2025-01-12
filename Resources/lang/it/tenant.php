cec14b7e83b98362e8d6b2c669f89317e7bb61f7
marco76tv <marco.sottana@gmail.com>
2025-01-12 21:29:04 +0100

    .

---
+++ Resources/lang/it/tenant.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'model' => 
+  array (
+    'label' => 'tenant.model',
+  ),
+  'plural' => 
+  array (
+    'model' => 
+    array (
+      'label' => 'tenant.plural.model',
+    ),
+  ),
+);
