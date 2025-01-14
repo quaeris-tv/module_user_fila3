d8e5829060ec899cbedf0b3787cda135db076a11
marco76tv <marco.sottana@gmail.com>
2025-01-14 11:43:11 +0100

    up

---
+++ Resources/lang/it/role.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'resources' => 'Risorse',
+    'pages' => 'Pagine',
+    'widgets' => 'Widgets',
+    'navigation' => [
+        'name' => 'Ruolo',
+        'plural' => 'Ruoli',
+        'group' => [
+            'name' => 'Admin',
+        ],
+    ],
+    'fields' => [
+        'name' => [
+            'label' => 'Nome',
+        ],
+        'guard_name' => [
+            'label' => 'Guard',
+        ],
+        'permissions' => 'Permessi',
+        'updated_at' => 'Aggiornato il',
+        'first_name' => 'Nome',
+        'last_name' => 'Cognome',
+        'select_all' => [
+            'name' => 'Seleziona Tutti',
+            'message' => '',
+        ],
+        'team' => [
+            'name' => [
+                'label' => 'team.name',
+            ],
+        ],
+    ],
+    'actions' => [
+        'import' => [
+            'fields' => [
+                'import_file' => 'Seleziona un file XLS o CSV da caricare',
+            ],
+        ],
+        'export' => [
+            'filename_prefix' => 'Aree al',
+            'columns' => [
+                'name' => 'Nome area',
+                'parent_name' => 'Nome area livello superiore',
+            ],
+        ],
+    ],
+];
