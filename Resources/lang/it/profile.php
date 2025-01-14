2bacccb7ffdfd20ec9b1c28e5ee86d071d43e3bc
marco76tv <marco.sottana@gmail.com>
2025-01-14 11:43:11 +0100

    up

---
+++ Resources/lang/it/profile.php
@@ -0,0 +1,90 @@
+<?php return array (
+  'account' => 'Account',
+  'profile' => 'Profilo',
+  'my_profile' => 'Il mio profilo',
+  'subheading' => 'Gestisci il tuo profilo.',
+  'fields' => 
+  array (
+    'type' => 
+    array (
+      'label' => 'Tipo',
+      'fields' => 
+      array (
+        'label' => 'Tipo',
+      ),
+    ),
+    'user' => 
+    array (
+      'name' => 
+      array (
+        'label' => 'Nome Utente',
+        'fields' => 
+        array (
+          'label' => 'Nome Utente',
+        ),
+      ),
+    ),
+    'photo' => 
+    array (
+      'label' => 'Foto',
+      'fields' => 
+      array (
+        'label' => 'Foto',
+      ),
+    ),
+    'id' => 
+    array (
+      'label' => 'id',
+    ),
+    'ente' => 
+    array (
+      'label' => 'ente',
+    ),
+    'matr' => 
+    array (
+      'label' => 'matr',
+    ),
+    'first_name' => 
+    array (
+      'label' => 'first_name',
+      'fields' => 
+      array (
+        'label' => 'first_name',
+      ),
+    ),
+    'last_name' => 
+    array (
+      'label' => 'last_name',
+      'fields' => 
+      array (
+        'label' => 'last_name',
+      ),
+    ),
+    'email' => 
+    array (
+      'fields' => 'email',
+      'label' => 
+      array (
+        'fields' => 'email',
+      ),
+    ),
+    'is_active' => 
+    array (
+      'fields' => 'is_active',
+      'label' => 
+      array (
+        'fields' => 'is_active',
+      ),
+    ),
+  ),
+  'personal_info' => 
+  array (
+    'heading' => 'Informazioni personali',
+    'subheading' => 'Gestisci le tue informazioni personali.',
+    'submit' => 
+    array (
+      'label' => 'Aggiorna',
+    ),
+    'notify' => 'Profilo aggiornato correttamente!',
+  ),
+);
--- Resources/lang/it/tenant.php
+++ Resources/lang/it/tenant.php
@@ -1,14 +1,52 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    'model' => [
-        'label' => 'tenant.model',
-    ],
-    'plural' => [
-        'model' => [
-            'label' => 'tenant.plural.model',
-        ],
-    ],
-];
+<?php return array (
+  'navigation' => 
+  array (
+    'name' => 'Tenant',
+    'plural' => 'Tenant',
+    'group' => 
+    array (
+      'name' => 'Admin',
+    ),
+  ),
+  'fields' => 
+  array (
+    'first_name' => 'Nome',
+    'last_name' => 'Cognome',
+    'secondary_color' => 
+    array (
+      'label' => 'secondary_color',
+    ),
+    'slug' => 
+    array (
+      'label' => 'slug',
+    ),
+    'name' => 
+    array (
+      'label' => 'name',
+    ),
+    'id' => 
+    array (
+      'label' => 'id',
+    ),
+  ),
+  'actions' => 
+  array (
+    'import' => 
+    array (
+      'fields' => 
+      array (
+        'import_file' => 'Seleziona un file XLS o CSV da caricare',
+      ),
+    ),
+    'export' => 
+    array (
+      'filename_prefix' => 'Aree al',
+      'columns' => 
+      array (
+        'name' => 'Nome area',
+        'parent_name' => 'Nome area livello superiore',
+      ),
+    ),
+    'change_password' => 'Cambio password',
+  ),
+);
