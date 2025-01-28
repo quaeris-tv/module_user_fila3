3c50dff06e384f4f2e17a1ecbcb151a5e1353655
marco76tv <marco.sottana@gmail.com>
2025-01-28 17:29:13 +0100

    up

--- config/config.php
+++ config/config.php
@@ -2,4 +2,19 @@
 
 declare(strict_types=1);
 
-return [];
+return [
+    'name' => 'User',
+    'description' => 'Modulo per la gestione degli utenti e autorizzazioni',
+    'icon' => 'heroicon-o-users',
+    'navigation' => [
+        'enabled' => true,
+        'sort' => 100,
+    ],
+    'routes' => [
+        'enabled' => true,
+        'middleware' => ['web', 'auth'],
+    ],
+    'providers' => [
+        'Modules\\User\\Providers\\UserServiceProvider',
+    ],
+];
