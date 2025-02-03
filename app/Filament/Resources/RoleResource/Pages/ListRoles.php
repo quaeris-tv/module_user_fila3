18398a6671a53f752af6508b7fef5d78404f5d35
marco76tv <1944941+marco76tv@users.noreply.github.com>
2025-02-03 20:40:04 +0000

    Check & fix styling

--- app/Filament/Resources/RoleResource/Pages/ListRoles.php
+++ app/Filament/Resources/RoleResource/Pages/ListRoles.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Modules\User\Filament\Resources\RoleResource\Pages;
 
-use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Actions\EditAction;
 use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\EditAction;
 use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
 use Modules\User\Filament\Resources\RoleResource;
 use Modules\User\Models\Role;
 use Modules\Xot\Filament\Resources\Pages\XotBaseListRecords;
