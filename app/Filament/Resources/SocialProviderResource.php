d70376db329efc056b11c82bd5a7e705ef5e8347
marco76tv <marco.sottana@gmail.com>
2025-02-03 20:04:32 +0100

    up

--- app/Filament/Resources/SocialProviderResource.php
+++ app/Filament/Resources/SocialProviderResource.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Modules\User\Filament\Resources;
 
-use Filament\Forms;
-use Filament\Forms\Form;
+use Filament\Forms\Components\KeyValue;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
 use Modules\User\Filament\Resources\SocialProviderResource\Pages;
 use Modules\User\Models\SocialProvider;
 use Modules\Xot\Filament\Resources\XotBaseResource;
@@ -14,14 +16,37 @@ class SocialProviderResource extends XotBaseResource
 {
     protected static ?string $model = SocialProvider::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
-
-    public static function form(Form $form): Form
+    public static function getFormSchema(): array
     {
-        return $form
-            ->schema([
-                Forms\Components\Toggle::make('active'),
-            ]);
+        return [
+            TextInput::make('name')
+                ->required()
+                ->maxLength(255)
+                ->placeholder(static::trans('fields.name.placeholder'))
+                ->helperText(static::trans('fields.name.helper_text')),
+
+            KeyValue::make('scopes')
+                ->placeholder(static::trans('fields.scopes.placeholder'))
+                ->helperText(static::trans('fields.scopes.helper_text')),
+
+            KeyValue::make('parameters')
+                ->placeholder(static::trans('fields.parameters.placeholder'))
+                ->helperText(static::trans('fields.parameters.helper_text')),
+
+            Toggle::make('stateless')
+                ->helperText(static::trans('fields.stateless.helper_text')),
+
+            Toggle::make('active')
+                ->helperText(static::trans('fields.active.helper_text')),
+
+            Toggle::make('socialite')
+                ->helperText(static::trans('fields.socialite.helper_text')),
+
+            Textarea::make('svg')
+                ->columnSpanFull()
+                ->placeholder(static::trans('fields.svg.placeholder'))
+                ->helperText(static::trans('fields.svg.helper_text')),
+        ];
     }
 
     public static function getRelations(): array
