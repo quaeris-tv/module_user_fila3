468d97e612a4610769409626ef4efd4c83407162
marco76tv <marco.sottana@gmail.com>
2025-02-03 19:39:43 +0100

    .

---
+++ lang/en/features.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'fields' => [
+        'name' => 'Name',
+        'name.placeholder' => 'Enter feature name',
+        'name.helper_text' => 'The name of the feature',
+        
+        'scope' => 'Scope',
+        'scope.placeholder' => 'Enter feature scope',
+        'scope.helper_text' => 'The scope of the feature (e.g. global, user, team)',
+        
+        'value' => 'Value',
+        'value.placeholder' => 'Enter feature value',
+        'value.helper_text' => 'The value or configuration of the feature',
+    ],
+]; 
---
+++ lang/it/features.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'fields' => [
+        'name' => 'Nome',
+        'name.placeholder' => 'Inserisci il nome della feature',
+        'name.helper_text' => 'Il nome della feature',
+        
+        'scope' => 'Ambito',
+        'scope.placeholder' => 'Inserisci l\'ambito della feature',
+        'scope.helper_text' => 'L\'ambito della feature (es. globale, utente, team)',
+        
+        'value' => 'Valore',
+        'value.placeholder' => 'Inserisci il valore della feature',
+        'value.helper_text' => 'Il valore o la configurazione della feature',
+    ],
+]; 
