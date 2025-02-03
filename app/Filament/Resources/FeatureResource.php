1c3bcee4857d0f2b55bf5a81ac5219622147b7fe
marco76tv <marco.sottana@gmail.com>
2025-02-03 19:34:20 +0100

    Auto update submodule

--- app/Filament/Resources/FeatureResource.php
+++ app/Filament/Resources/FeatureResource.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\User\Filament\Resources;
 
-use Filament\Forms\Form;
+use Filament\Forms\Components\TextInput;
 // use Filament\Forms;
 use Modules\User\Filament\Resources\FeatureResource\Pages;
 use Modules\User\Models\Feature;
@@ -17,13 +17,30 @@ class FeatureResource extends XotBaseResource
 {
     protected static ?string $model = Feature::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
-
-    public static function form(Form $form): Form
+    public static function getFormSchema(): array
     {
-        return $form
-            ->schema([
-            ]);
+        return [
+            TextInput::make('name')
+                ->required()
+                ->maxLength(255)
+                ->label('Name')
+                ->placeholder('Enter feature name')
+                ->helperText('The name of the feature'),
+
+            TextInput::make('scope')
+                ->required()
+                ->maxLength(255)
+                ->label('Scope')
+                ->placeholder('Enter feature scope')
+                ->helperText('The scope of the feature (e.g. global, user, team)'),
+
+            TextInput::make('value')
+                ->required()
+                ->maxLength(255)
+                ->label('Value')
+                ->placeholder('Enter feature value')
+                ->helperText('The value or configuration of the feature'),
+        ];
     }
 
     public static function getRelations(): array
