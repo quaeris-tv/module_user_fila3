20e455693e6cbb103debfeb46300b1c1f6a8bff7
Malebestia <vair81@gmail.com>
2025-02-04 08:11:07 +0100

    Here is a possible commit message for this commit:
    "Updated translations and added new actions to the user management modal. This includes the ability to associate, detach, and delete users in bulk."
    
    This commit message is concise and informative, providing a clear summary of the changes made in this commit. It also highlights the key features that were added or updated, which can be helpful for other developers who may need to understand the context of this change.

--- lang/it/feature.php
+++ lang/it/feature.php
@@ -1,45 +1,53 @@
-<?php
-
-return [
-    'resources' => 'Risorse',
-    'pages' => 'Pagine',
-    'widgets' => 'Widgets',
-    'navigation' => [
-        'name' => 'Feature',
-        'plural' => 'Features',
-        'group' => [
-            'name' => 'Admin',
-        ],
-        'label' => 'feature',
-        'sort' => 18,
-    ],
-    'fields' => [
-        'name' => 'Nome',
-        'guard_name' => 'Guard',
-        'permissions' => 'Permessi',
-        'updated_at' => 'Aggiornato il',
-        'first_name' => 'Nome',
-        'last_name' => 'Cognome',
-        'select_all' => [
-            'name' => 'Seleziona Tutti',
-            'message' => '',
-        ],
-        'value' => [
-            'label' => 'value',
-        ],
-    ],
-    'actions' => [
-        'import' => [
-            'fields' => [
-                'import_file' => 'Seleziona un file XLS o CSV da caricare',
-            ],
-        ],
-        'export' => [
-            'filename_prefix' => 'Aree al',
-            'columns' => [
-                'name' => 'Nome area',
-                'parent_name' => 'Nome area livello superiore',
-            ],
-        ],
-    ],
-];
+<?php return array (
+  'resources' => 'Risorse',
+  'pages' => 'Pagine',
+  'widgets' => 'Widgets',
+  'navigation' => 
+  array (
+    'name' => 'Feature',
+    'plural' => 'Features',
+    'group' => 
+    array (
+      'name' => 'Admin',
+    ),
+    'label' => 'feature',
+    'sort' => 42,
+  ),
+  'fields' => 
+  array (
+    'name' => 'Nome',
+    'guard_name' => 'Guard',
+    'permissions' => 'Permessi',
+    'updated_at' => 'Aggiornato il',
+    'first_name' => 'Nome',
+    'last_name' => 'Cognome',
+    'select_all' => 
+    array (
+      'name' => 'Seleziona Tutti',
+      'message' => '',
+    ),
+    'value' => 
+    array (
+      'label' => 'value',
+    ),
+  ),
+  'actions' => 
+  array (
+    'import' => 
+    array (
+      'fields' => 
+      array (
+        'import_file' => 'Seleziona un file XLS o CSV da caricare',
+      ),
+    ),
+    'export' => 
+    array (
+      'filename_prefix' => 'Aree al',
+      'columns' => 
+      array (
+        'name' => 'Nome area',
+        'parent_name' => 'Nome area livello superiore',
+      ),
+    ),
+  ),
+);
