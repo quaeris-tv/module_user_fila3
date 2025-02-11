3ef83f9173301c41c23471a479bb6a340babc4bf
marco76tv <marco.sottana@gmail.com>
2025-02-11 08:47:32 +0100

    up

---
+++ App/Models/ProfileTeam.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\User\Models;
+
+use Modules\Xot\Contracts\ProfileContract;
+
+/**
+ * @property ProfileContract|null $creator
+ * @property ProfileContract|null $updater
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|ProfileTeam newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|ProfileTeam newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|ProfileTeam query()
+ *
+ * @mixin \Eloquent
+ */
+class ProfileTeam extends TeamUser {}
