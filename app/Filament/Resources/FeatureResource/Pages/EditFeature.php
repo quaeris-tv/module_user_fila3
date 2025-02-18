674144a3953843ebd6e76bfe3ccb7fbe35ba6ad0
marco76tv <1944941+marco76tv@users.noreply.github.com>
2025-02-18 20:46:27 +0000

    Check & fix styling

--- app/Filament/Resources/FeatureResource/Pages/EditFeature.php
+++ app/Filament/Resources/FeatureResource/Pages/EditFeature.php
@@ -4,12 +4,9 @@ declare(strict_types=1);
 
 namespace Modules\User\Filament\Resources\FeatureResource\Pages;
 
-use Filament\Actions;
 use Modules\User\Filament\Resources\FeatureResource;
 
 class EditFeature extends \Modules\Xot\Filament\Resources\Pages\XotBaseEditRecord
 {
     protected static string $resource = FeatureResource::class;
-
-   
 }
--- app/Filament/Resources/ProfileResource/Pages/EditProfile.php
+++ app/Filament/Resources/ProfileResource/Pages/EditProfile.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 namespace Modules\User\Filament\Resources\ProfileResource\Pages;
 
-use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 use Modules\User\Filament\Resources\ProfileResource;
 
 class EditProfile extends EditRecord
 {
     protected static string $resource = ProfileResource::class;
-
-   
 }
