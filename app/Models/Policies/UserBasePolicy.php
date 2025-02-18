1dd3453df9cba3df03e3eeed47c8f76649c27776
marco76tv <1944941+marco76tv@users.noreply.github.com>
2025-02-18 19:05:30 +0000

    Check & fix styling

--- app/Models/Policies/UserBasePolicy.php
+++ app/Models/Policies/UserBasePolicy.php
@@ -2,16 +2,14 @@
 
 declare(strict_types=1);
 
-
 namespace Modules\User\Models\Policies;
 
 use Illuminate\Auth\Access\HandlesAuthorization;
 use Modules\Xot\Contracts\UserContract;
 use Modules\Xot\Datas\XotData;
 
-
 /**
- * ---
+ * ---.
  */
 abstract class UserBasePolicy
 {
