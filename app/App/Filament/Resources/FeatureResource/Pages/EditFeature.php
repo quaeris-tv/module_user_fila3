11b0901cc343780bd10e75ccb55adc297462e8ba
marco76tv <1944941+marco76tv@users.noreply.github.com>
2025-02-18 20:46:27 +0000

    Check & fix styling

--- app/App/Filament/Resources/FeatureResource/Pages/EditFeature.php
+++ app/App/Filament/Resources/FeatureResource/Pages/EditFeature.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 namespace Modules\User\Filament\Resources\FeatureResource\Pages;
 
-use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 use Modules\User\Filament\Resources\FeatureResource;
 
 class EditFeature extends EditRecord
 {
     protected static string $resource = FeatureResource::class;
-
-   
 }
